e50a358c87ebbdb92a5f7fa05df173935d0d91c1
Eniolasoar <eniolanishe@gmail.com>
2023-09-13 15:31:43 +0100

    First Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import './Header.css'
+
+function Header(){
+    return(
+        <>
+            <section className="header" >
+                <nav>
+                    <div className="nav-left">
+                        <img src="src/Icons/tv.png" alt="" className="logo"/>
+                        <p>MovieBox</p>
+                    </div>
+                    <div className="input-container">
+                        <input type="text" placeholder="Search"/>
+                        <img src="src/Icons/search.png" alt=""/>
+                    </div>
+                    <div className="nav-right">
+                        <p className="signIn">Sign in</p>
+                        <div className="menu">
+                            <img src="src/Icons/Menu alt 4.svg" alt="" />
+                        </div>
+            
+                    </div>
+                </nav>
+                <div className="text">
+                    <h1>John Wick 3:<br/>Parabellum</h1>
+                    <div className="rating">
+                        <div className="imdb ">
+                            <img src="src/Icons/IMDB.png" alt=""/>
+                            86.0/100
+                        </div>
+                        <div className="rottenTomatoes">
+                            <img src="src/Icons/tomato.png" alt="" />
+                            97%
+                        </div>
+                    </div>
+                    <p className="paragraph">John Wick is on the run after killing a member of the international assassins' guild, and with a $14 million price tag on his head, he is the target of hit men and women everywhere.</p>
+                    <button>
+                        <img src="src/Icons/play.png" alt=""/>
+                        <p>WATCH TRAILER</p>
+                    </button>
+                </div>
+                </section>
+        </>
+    )
+    
+}
+export default Header
